e8deeb427101161e6a0751796c61707c3a5c38ec
mansijoshi17 <mansijoshi1777@gmail.com>
2021-11-27 11:19:39 +0530

    Changed configration to ploygon network

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const nftaddress = "0xefB636cE3c007696A1B88CfC3aAbbFD183243C16"
-export const nftmarketaddress = "0xc231C7a1f280a1a6Cc0d09f6bE5713bafae6D5C8"
+export const nftaddress = "0x1Ed37a36dD70Ae2039d00986f3E0402823F5e9f0"
+export const nftmarketaddress = "0xFB835c81361101Ec222608275006693E93D6F8db"
 export const useraddresss= "0xacAfAF939202a5b9B8d76a8F97D656F32a559CdB";
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,14 @@ require("@nomiclabs/hardhat-waffle");
 
 const fs = require("fs");
 const privateKey = fs.readFileSync(".secret").toString();
-const projectId = "0625a19e9662496eac15a4d713be4eb9"
+const ALCHEMY_API_KEY = "pqr-VAfSii011IkFlqrZTxCgzK5fWegs";
+
 
 module.exports = {
   solidity: "0.8.4",
   networks:{
-    ropsten:{
-      url:`https://ropsten.infura.io/v3/${projectId}`,
+    matic:{
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts:[privateKey],
       gas: 2100000,
       gasPrice: 8000000000
